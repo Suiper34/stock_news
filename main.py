99c7fd2d52e5c8ad6af8ef07a5099abeedf2aeb1
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-06-11 12:49:53 +0000

    get yesterday and eve to yesterday dates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime, timedelta
 
 import requests
 
@@ -11,9 +12,14 @@ parameters = {
 }
 
 stock_response = requests.get(
-    url="https://www.alphavantage.co.query", params=parameters)
+    url="https://www.alphavantage.co/query", params=parameters)
 stock_response.raise_for_status()
 
-daily_stocks = stock_response.json()
+daily_stocks = stock_response.json()['Time Series (Daily)']
 
 print(daily_stocks)
+
+# use datetime and timedelta to get yesterday and the day before yesterday
+yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+day_before_yesterday = (
+    datetime.now() - timedelta(days=2)).strftime("%Y-%m-%d")
